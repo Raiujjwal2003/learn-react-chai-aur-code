2983873850d8bf0228a5c8213796a32d1dd43887
Raiujjwal2003 <raiujjwal2003@gmail.com>
2023-10-22 00:27:48 +0530

    update repo

---
+++ 01basicreactnormal/src/App.js
@@ -0,0 +1,13 @@
+
+import Name from "./name";
+
+function App() {
+  return (
+    <>
+    <Name />
+    <h1> hey there that is my first project with  normal react</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreactnormal/src/name.js
@@ -0,0 +1,7 @@
+function Name(){
+    return(
+        <h2> hey ujjwal that function return in App.js</h2>
+    )
+}
+
+export default Name;
